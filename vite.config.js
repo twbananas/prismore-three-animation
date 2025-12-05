8e17d8ab07a89c5cc8dca6d78b586dc160cb8a1f
twbananas <info@twbananas.com>
2025-12-05 16:16:26 +0000

    Fix: externalize Draco modules

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ export default {
   build: {
     rollupOptions: {
       external: [
-        "three/examples/jsm/loaders/DRACOLoader.js"
+        "three/examples/jsm/loaders/DRACOLoader.js",
+        "three/examples/jsm/libs/draco/"
       ]
     }
   }
